1c538b6aea063123aef09ebe288776594be3e6c7
TheOldZoom <xabou2010@gmail.com>
2025-07-05 19:33:59 -0600

    mv ./bot/ to ./

--- bot/src/events/ready.ts
+++
@@ -1,8 +0,0 @@
-import { Events, Client } from "discord.js";
-import { Event } from "../classes/Event";
-import logger from "../utils/logger";
-
-export default Event(Events.ClientReady, (client: Client) => {
-  logger.info(`Logged in successfully as ${client.user?.tag}`);
-  logger.info(`Serving ${client.guilds.cache.size} guilds`);
-});
--- bot/src/index.ts
+++
@@ -1,9 +0,0 @@
-import "dotenv/config";
-import { client } from "./classes/Client";
-import logger from "./utils/logger";
-
-async function main() {
-  await client.login();
-}
-
-main();
